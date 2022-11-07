d41e2a768bf1143679c86273d20d79e6f1dc6b4b
BradleyK <bradleyjkennett@gmail.com>
2022-11-07 16:41:35 -0500

    upload image feature add

--- server/server.js
+++ server/server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 const userRoutes = require('./routes/user-routes');
+const imageRoutes = require('./routes/image-uploads');
 
 // express middleware, used to be bodyparser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 
 // Serve up static assets
 if (process.env.NODE_ENV === 'production') {
@@ -14,6 +16,7 @@ if (process.env.NODE_ENV === 'production') {
 
 // app.use(require('./routes'));
 app.use('/api/', userRoutes);
+app.use('/api/', imageRoutes);
 
 // Start the API server
 app.listen(PORT, () =>
---
+++ server/utils/params-config.js
@@ -0,0 +1,17 @@
+const { v4: uuidv4 } = require('uuid');
+
+const params = (fileName) => {
+    const myFile = fileName.originalname.split('.');
+    const fileType = myFile[myFile.length - 1];
+
+
+    const imageParams = {
+        Bucket: 'user-images-5e8ff81d-45cf-4094-9844-47ffc1fc7002',
+        Key: `${uuidv4()}.${fileType}`,
+        Body: fileName.buffer,
+    };
+
+    return imageParams;
+};
+
+module.exports = params;
