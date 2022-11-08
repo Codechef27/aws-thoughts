f9754a0f2f08a8c584f0e6711d88b1176322932c
BradleyK <bradleyjkennett@gmail.com>
2022-11-08 15:55:49 -0500

    retriev imgs from s3 bucket add

--- server/routes/user-routes.js
+++ server/routes/user-routes.js
@@ -33,11 +33,12 @@ router.get("/users/:username", (req, res) => {
       "#un": "username",
       "#ca": "createdAt",
       "#th": "thought",
+      "#img": "image"
     },
     ExpressionAttributeValues: {
       ":user": req.params.username,
     },
-    ProjectionExpression: "#th, #ca, #un",
+    ProjectionExpression: "#un, #th, #ca, #img",
     ScanIndexForward: false,
   };
 
@@ -61,6 +62,7 @@ router.post('/users', (req, res) => {
             username: req.body.username,
             createdAt: Date.now(),
             thought: req.body.thought,
+            image: req.body.image
         },
     };
     //db call 
--- server/utils/params-config.js
+++ server/utils/params-config.js
@@ -9,6 +9,7 @@ const params = (fileName) => {
         Bucket: 'user-images-5e8ff81d-45cf-4094-9844-47ffc1fc7002',
         Key: `${uuidv4()}.${fileType}`,
         Body: fileName.buffer,
+        ACL: 'public-read', // allow read access to the files. 
     };
 
     return imageParams;
